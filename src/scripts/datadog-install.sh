3a947188b0cd7143473e43703afc4824f90398a3
Phillip Chen <phillip.chen@skykick.com>
2024-09-27 10:07:56 -0700

    modify datadog script to ensure only the main tag section gets updated

--- src/scripts/datadog-install.sh
+++ src/scripts/datadog-install.sh
@@ -82,6 +82,7 @@ log "Installing DataDog plugin onto this machine using API Key [$API_KEY]"
 
 wget -q https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh -O $DD_AGENT
 chmod +x $DD_AGENT
+sed -i 's|/# tags:.\*|/^# tags:.*$|g' $DD_AGENT
 DD_API_KEY=$API_KEY DD_AGENT_MAJOR_VERSION=7 DD_HOSTNAME=$DATADOG_HOSTNAME DD_HOST_TAGS=$TAGS $DD_AGENT 
 rm /etc/datadog-agent/conf.d/elastic.d/auto_conf.yaml
 
